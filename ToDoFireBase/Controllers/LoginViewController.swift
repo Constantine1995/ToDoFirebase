80daeb1d52371c3bea15ee474b9695111ab214f8
Kostya Likhachev <arrow.koss@gmail.com>
2019-04-20 20:02:53 +0300

    - fixed display of UI and keyboard

--- ToDoFireBase/Controllers/LoginViewController.swift
+++ ToDoFireBase/Controllers/LoginViewController.swift
@@ -11,18 +11,34 @@ import UIKit
 class LoginViewController: UIViewController {
 
     @IBOutlet weak var warningLabel: UILabel!
-    
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
-     
-    @IBAction func loginTapped(_ sender: Any) {
-    }
-    @IBAction func registerTapped(_ sender: Any) {
-    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        NotificationCenter.default.addObserver(self, selector: #selector(kbDidShow), name: UIResponder.keyboardDidShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(kbDidHide), name: UIResponder.keyboardDidHideNotification, object: nil)
     }
+    
+    @objc func kbDidShow(notifcation: Notification) {
+        guard let userInfo = notifcation.userInfo else { return }
+        let kbFrameSize = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        (self.view as! UIScrollView).contentSize = CGSize(width: self.view.bounds.size.width, height: self.view.bounds.size.height + kbFrameSize.height)
+         (self.view as! UIScrollView).scrollIndicatorInsets = UIEdgeInsets(top: 0, left: 0, bottom: kbFrameSize.height, right: 0)
+    }
+    
+    @objc func kbDidHide() {
+        (self.view as! UIScrollView).contentSize = CGSize(width: self.view.bounds.size.width, height: self.view.bounds.size.height)
+
+    }
+    
+    @IBAction func loginTapped(_ sender: Any) {
+    }
+    
+    @IBAction func registerTapped(_ sender: Any) {
+    }
+    
+ 
 
 
 }
